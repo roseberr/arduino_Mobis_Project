da6e42854898cd2f0f53dbbf90b7df36701bf1ed
solchan_bhang <bsc981111@naver.com>
2023-02-23 23:49:45 -0800

    Update TEST.ino

--- TEST/TEST.ino
+++ TEST/TEST.ino
@@ -1,8 +1,16 @@
 #include "LedControl.h"
-
+#include <SoftwareSerial.h>
+#include <AFMotor.h>
 #define sensor A4
 
-LedControl dot = LedControl(12,10,11,1);
+// 0 - WHITE, 1 - BLACK
+// motor : 2 - left, 1 - right
+// sensor : a0(val1) - left, a5(val2) - right
+// motor_L speed :105 ,motor_R spedd:85
+int left_val,right_val;
+AF_DCMotor motor_L(3);
+AF_DCMotor motor_R(4);
+LedControl dot = LedControl(15,17,16,1);
 
 byte heart[] = {
   B00000000,
@@ -38,11 +46,12 @@ byte smile[] = {
   B00111100
 };  
 
+
 byte forward[] = {
   B00011000,
   B00111100,
   B01111110,
-  B00011000,
+  B11111111,
   B00011000,
   B00011000,
   B00011000,
@@ -76,27 +85,86 @@ byte backward[] = {
   B00011000,
   B00011000,
   B00011000,
-  B00011000,
+  B11111111,
   B01111110,
   B00111100,
   B00011000
 };
 
-void setup(){
+void setup() {
+  // setting for motor
+  motor_L.setSpeed(230);
+  motor_L.run(RELEASE);
+  motor_R.setSpeed(200);
+  motor_R.run(RELEASE);
+
+  // setting for dot matrix
   dot.shutdown(0, false);
   dot.setIntensity(0,5);
   dot.clearDisplay(0);
-  Serial.begin(9600); // Start serial
+
+  // setting for IR sensor
+  
+//  Serial.begin(9600);
+//  pinMode(A0, INPUT);
+//  pinMode(A5, INPUT);
 }
 
-void loop(){
-  Serial.print("1.sensor :");
+void loop() {
+  left_val=digitalRead(A0); 
+  right_val=digitalRead(A5);
+
+//  Serial.print(val1);
+//  Serial.println(val2);
+//  delay(1000);
+
+  /*
   int sensorValue = analogRead(sensor);
-  Serial.println(sensorValue);
-  //delay(1000);
-  
-  showmatrix_right(right_curve);
-  delay(200);
+  // blocked by something
+  if (sensorValue > 100){
+    motor_L.run(RELEASE);
+    motor_R.run(RELEASE);
+    showmatrix(x);
+  }*/
+
+  // go forward
+  if (left_val == 0 && right_val == 0){
+      motor_L.run(FORWARD);
+      motor_R.run(FORWARD);
+      showmatrix(forward);
+      //delay(10);
+      }
+
+  // RIGHT CURVE
+  else if (left_val == 0 && right_val == 1){
+      motor_L.run(FORWARD);
+      motor_R.run(BACKWARD);
+      //motor_R.run(RELEASE); 
+      showmatrix(right_curve);
+      //delay(10);
+  }
+
+  // LEFT CURVE
+  else if (left_val == 1 && right_val == 0){
+      motor_L.run(BACKWARD);
+      //motor_L.run(RELEASE);
+      motor_R.run(FORWARD);
+
+      showmatrix(left_curve);
+      //delay(10);
+  }
+  else{
+    showmatrix(smile);
+    //delay(50);
+  }
+  //delay(50);
+
+  /*
+  else if (left_val == 1 && right_val == 1){
+      motor_L.run(RELEASE);
+      motor_R.run(RELEASE);
+     // delay(200);
+  }*/
 }
 
 void showmatrix_left(byte arr[]){
@@ -129,20 +197,31 @@ void showmatrix_right(byte arr[]){
   }
 }
 
-/*
-void showmatrix_up(byte arr[]){
-  int n = 0;
-  for(int i=n; i<8; i++){
-    dot.setRow(0,i,arr[i]);
-  }
-  
-}*/
 
-void blinkmatrix(byte arr[]){
-  delay(1000);
+void showmatrix_up(byte arr[]){
   for(int i=0; i<8; i++){
     dot.setRow(0,i,arr[i]);
   }
-  delay(500);
-  dot.clearDisplay(0);
+
+  for(int i=0; i<8; i++){
+     arr[i] = arr[(i+1)%8];
+  }
+}
+
+/*
+void showmatrix_down(byte arr[]){
+  for(int i=0; i<8; i++){
+    dot.setRow(0,i,arr[i]);
+  }
+
+  for(int i=0; i<8; i++){
+     arr[i] = arr[(i+7)%8];
+  }
+}*/
+
+void showmatrix(byte arr[]){
+  for(int i=0; i<8; i++){
+    dot.setRow(0,i,arr[i]);
+  }
+  delay(10);
 }
