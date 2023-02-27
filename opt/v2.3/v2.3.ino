822a6f3c23a30d80c1dc4ee2bab0de4a362d2db8
solchan_bhang <bsc981111@naver.com>
2023-02-26 23:26:21 -0800

    Update v2.3.ino
    updated register code

--- opt/v2.3/v2.3.ino
+++ opt/v2.3/v2.3.ino
@@ -88,11 +88,6 @@ void run(int motorNum,int cmd);
 
 void motorConstruct();
 
-
-
-
-
-
 //AF_DCMotor motor_L(1);              // 모터드라이버 L293D  1: M1에 연결,  4: M4에 연결
 //AF_DCMotor motor_R(4); 
 
@@ -104,9 +99,9 @@ void setup() {
 
   // turn on motor
 
-  setSpeed(leftMotorNum,200);
+  setSpeed(leftMotorNum,240);
   run(leftMotorNum,RELEASE);
-  setSpeed(rightMotorNum,150);
+  setSpeed(rightMotorNum,180);
   run(rightMotorNum,RELEASE);
   
   //motor_L.setSpeed(230);              // 왼쪽 모터의 속도   
@@ -123,24 +118,24 @@ void loop() {
        //motor_L.run(FORWARD); 
        //motor_R.run(FORWARD);
        
-        setSpeed(leftMotorNum,200);
-        setSpeed(rightMotorNum,150);
+        setSpeed(leftMotorNum,240);
+        setSpeed(rightMotorNum,180);
         run(leftMotorNum , FORWARD);
         run(rightMotorNum , FORWARD);
       }
       else if (val1 == 0 && val2 == 1) {              // 우회전
        //motor_L.run(FORWARD); 
        //motor_R.run(RELEASE);
-        setSpeed(leftMotorNum,150);
-        setSpeed(rightMotorNum,170);
+        setSpeed(leftMotorNum,170);
+        setSpeed(rightMotorNum,200);
         run(leftMotorNum , FORWARD);
         run(rightMotorNum , BACKWARD);
       }
       else if (val1 == 1 && val2 == 0) {              // 좌회전
         //motor_L.run(RELEASE); 
         //motor_R.run(FORWARD);
-        setSpeed(leftMotorNum,170);
-        setSpeed(rightMotorNum,150);
+        setSpeed(leftMotorNum,200);
+        setSpeed(rightMotorNum,170);
         run(leftMotorNum,BACKWARD);
         run(rightMotorNum , FORWARD);
       } 
@@ -156,28 +151,28 @@ void loop() {
 void latch_tx() { //reset
   int i;
 
-  //LATCH_PORT &= ~_BV(LATCH);
-  digitalWrite(MOTORLATCH, LOW);
+  LATCH_PORT &= ~_BV(LATCH);
+  //digitalWrite(MOTORLATCH, LOW);
 
-  //SER_PORT &= ~_BV(SER);
-  digitalWrite(MOTORDATA, LOW);
+  SER_PORT &= ~_BV(SER);
+  //digitalWrite(MOTORDATA, LOW);
 
   for (i=0; i<8; i++) {
-    //CLK_PORT &= ~_BV(CLK);
-    digitalWrite(MOTORCLK, LOW);
+    CLK_PORT &= ~_BV(CLK);
+    //digitalWrite(MOTORCLK, LOW);
 
     if (latch_state & _BV(7-i)) {
-      //SER_PORT |= _BV(SER);
-      digitalWrite(MOTORDATA, HIGH);
+      SER_PORT |= _BV(SER);
+      //digitalWrite(MOTORDATA, HIGH);
     } else {
-      //SER_PORT &= ~_BV(SER);
-      digitalWrite(MOTORDATA, LOW);
+      SER_PORT &= ~_BV(SER);
+      //digitalWrite(MOTORDATA, LOW);
     }
-    //CLK_PORT |= _BV(CLK);
-    digitalWrite(MOTORCLK, HIGH);
+    CLK_PORT |= _BV(CLK);
+    //digitalWrite(MOTORCLK, HIGH);
   }
-  //LATCH_PORT |= _BV(LATCH);
-  digitalWrite(MOTORLATCH, HIGH);
+  LATCH_PORT |= _BV(LATCH);
+  //digitalWrite(MOTORLATCH, HIGH);
 }
 
 inline void initPWM1(uint8_t freq){
@@ -192,7 +187,7 @@ inline void setPWM1(uint8_t s){
 }
 
 inline void initPWM2(uint8_t freq) {
-      // use PWM from timer2B (pin 3)
+    // use PWM from timer2B (pin 3)
     TCCR2A |= _BV(COM2B1) | _BV(WGM20) | _BV(WGM21); // fast PWM, turn on oc2b
     TCCR2B = freq & 0x7;
     OCR2B = 0;
@@ -216,7 +211,6 @@ inline void initPWM3(uint8_t freq) {
 }
 
 inline void setPWM3(uint8_t s) {
-  
     // use PWM from timer0A on PB3 (Arduino pin #6)
     OCR0A = s;
 }
@@ -225,15 +219,12 @@ inline void initPWM4(uint8_t freq){
     TCCR0A |= _BV(COM0B1) | _BV(WGM00) | _BV(WGM01); // fast PWM, turn on oc0a
     //TCCR0B = freq & 0x7;
     OCR0B = 0;
-
     pinMode(5, OUTPUT);
 }
 
 inline void setPWM4(uint8_t s) {
-    
     // use PWM from timer0A on PB3 (Arduino pin #6)
     OCR0B = s;
-  
 }
 
 void setSpeed(int motorNum,int speed){
@@ -305,8 +296,8 @@ void motorConstruct(){
 
 
 
-  //ENABLE_PORT &= ~_BV(ENABLE); // enable the chip outputs!
-  digitalWrite(MOTORENABLE, LOW);
+  ENABLE_PORT &= ~_BV(ENABLE); // enable the chip outputs!
+  //digitalWrite(MOTORENABLE, LOW);
   
   //afmotor controller default constructor end
   
